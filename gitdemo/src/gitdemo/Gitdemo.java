37b80afba99c8eb8902888267c2bfd926655dbe3
sanjida <sanjida@LAPTOP-D4F7AOBL>
2022-09-17 12:02:17 +0530

    My first commit

---
+++ gitdemo/src/gitdemo/Gitdemo.java
@@ -0,0 +1,12 @@
+package gitdemo;
+
+public class Gitdemo {
+
+	public static void main(String[] args) {
+		System.out.println("HAI GIT");
+		System.out.println("HAI");
+		// TODO Auto-generated method stub
+
+	}
+
+}
